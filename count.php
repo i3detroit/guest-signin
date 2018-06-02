ed51d4ea860f6579b057ba27acc73a397cb0cc90
Mark Furland <mtfurlan@i3detroit.org>
2018-06-01 21:45:13 -0400

    count.php counts by year

--- count.php
+++ count.php
@@ -4,13 +4,24 @@
     require 'password.php';
     $conn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDB);
 
-    $sql = "SELECT COUNT(*) AS count FROM waiver_data";
+    $sql = "SELECT DATE_FORMAT(timestamp, '%Y') as 'year',
+	COUNT(id) as 'total'
+	FROM waiver_data
+	GROUP BY DATE_FORMAT(timestamp, '%Y')";
     $result = $conn->query($sql);
 
     echo "<pre>";
-    printf("%d entries.", $result->fetch_assoc()["count"]);
+    if($result) {
+	#printf("%d entries.", $result->fetch_assoc()["count"]);
+	while ($row = $result->fetch_assoc()) {
+	    print_r($row);
+	}
+    } else {
+	echo "query failed";
+	print_r($conn->error);
+    }
     echo "</pre>";
 
-    $result->close();
+    $conn->close();
     require 'footer.php';
 
